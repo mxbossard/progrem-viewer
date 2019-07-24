6c54934f8bb299a730b84ec6b1c4e1a02bc0cd43
Maxime BOSSARD <mxbossard@gmail.com>
2019-07-24 12:02:11 +0200

    Add a gradient progrem.

---
+++ dist/progrems/gradient_progrem.js
@@ -0,0 +1,13 @@
+function rgbColor(r, g, b) {
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function initialiserProgrem(config, initContexte) {
+    config.nombreFrames = 32;
+}
+
+function colorerProgrem(colonne, ligne, frame, contexte) {
+    redColor = Math.abs(frame * 16 % 510 - 254);
+    color = rgbColor(redColor, colonne * 255 / 17, ligne * 255 / 17);
+    return color;
+}
